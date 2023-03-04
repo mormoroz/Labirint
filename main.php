a8ae5c37df427666869bb1aed57381df1f3191c3
Alexander Morozov <Alexander95223@yandex.ru>
2023-03-04 17:49:48 +0300

    feat(PHP): added console input (issue-3)

--- main.php
+++ main.php
@@ -53,10 +53,45 @@ function printShortestPath($from_name, $to_name, $routes): void
 }
 
 $routes = array();
-$routes[] = array('from'=>'a', 'to'=>'b', 'price'=>0);
-$routes[] = array('from'=>'c', 'to'=>'d', 'price'=>3);
-$routes[] = array('from'=>'b', 'to'=>'c', 'price'=>2);
-$routes[] = array('from'=>'a', 'to'=>'d', 'price'=>9);
-$routes[] = array('from'=>'b', 'to'=>'d', 'price'=>3);
+echo"Input size of matrix [N][M]:".PHP_EOL;
+echo "N= ";
+$n = readline();
+echo PHP_EOL."M= ";
+$m = readline();
+$id = 1;
 
-printShortestPath('a', 'd', $routes);
+for ($i = 0; $i < $n; $i++){
+    for ($j = 0; $j < $m-1; $j++){
+        echo "number of moves from A[", $i,"]","[", $j,"] (id = ",$id, ") to ", "A[", $i,"]","[", $j+1,"] (id = ",$id+1,"):".PHP_EOL;
+        $price = readline();
+        $routes[] = array('from'=>$id, 'to'=>($id+1), 'price'=>$price);
+        $id++;
+    }
+    $id++;
+}
+$id = 1;
+for ($j = 0; $j < $m; $j++){
+    for ($i = 0; $i < $n-1; $i++){
+        echo "number of moves from A[", $i,"]","[", $j,"] (id = ",$id, ") to ", "A[", $i+1,"]","[", $j,"] (id = ",$id+$m,"):".PHP_EOL;
+        $price = readline();
+        $routes[] = array('from'=>$id, 'to'=>($id+$m), 'price'=>$price);
+        $routes[] = array('from'=>($id+$m), 'to'=>$id, 'price'=>$price);
+        $id = $id + $m;
+    }
+    $id = $j+2;
+}
+
+echo "Input id of start point: ";
+$start = readline();
+
+echo "Input id of end point: ";
+$end = readline();
+
+//print_r($routes);
+//$routes[] = array('from'=>'a', 'to'=>'b', 'price'=>0);
+//$routes[] = array('from'=>'c', 'to'=>'d', 'price'=>3);
+//$routes[] = array('from'=>'b', 'to'=>'c', 'price'=>2);
+//$routes[] = array('from'=>'a', 'to'=>'d', 'price'=>9);
+//$routes[] = array('from'=>'b', 'to'=>'d', 'price'=>3);
+
+printShortestPath($start, $end, $routes);
